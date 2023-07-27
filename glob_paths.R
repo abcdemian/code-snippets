c8725325fcfa7e4f8d5ff7645a3355ab027f3648
Demian Grams <57306593+demiangrams@users.noreply.github.com>
2023-07-27 20:27:19 -0300

    Create glob_paths.R

---
+++ glob_paths.R
@@ -0,0 +1,4 @@
+# get the full path to every file that has .csv extension located at some predefined folder/path
+paths <- Sys.glob(paths = "C:/Users/grams/myfavoritefolder/*.csv")
+
+# obviously you can give the `paths` argument in many different ways
